f989003df722ad8eedfb9f3df8e9e553ce240b29
Eric Rushing <rushinge@ucsb.edu>
2019-10-14 14:58:48 -0700

    Allow consumer to specify bucket deletion policy

--- src/resource/destroyable-bucket.ts
+++ src/resource/destroyable-bucket.ts
@@ -1,15 +1,12 @@
 import { Code, Runtime, SingletonFunction } from '@aws-cdk/aws-lambda'
-import { Construct, RemovalPolicy, Duration } from '@aws-cdk/core'
+import { Construct, Duration } from '@aws-cdk/core'
 import { CustomResource, CustomResourceProvider} from '@aws-cdk/aws-cloudformation'
 import { Bucket, BucketProps } from '@aws-cdk/aws-s3'
 import path = require('path')
 
 export class DestroyableBucket extends Bucket {
   constructor(scope: Construct, id: string, props: BucketProps = {}) {
-    super(scope, id, {
-      ...props,
-      removalPolicy: RemovalPolicy.DESTROY
-    })
+    super(scope, id, {...props})
 
     const lambda = new SingletonFunction(this, 'DestroyableBucketHandler', {
       uuid: 'a8e853be-c06c-46c6-ab64-bd61819e992f',
