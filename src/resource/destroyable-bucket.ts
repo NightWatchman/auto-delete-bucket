4d7f88442762c67f9df0cbac159e499e38398912
Eric Rushing <rushinge@ucsb.edu>
2020-10-28 16:45:53 -0700

    Use new CustomResource API

--- src/resource/destroyable-bucket.ts
+++ src/resource/destroyable-bucket.ts
@@ -1,6 +1,4 @@
-import { Code, Runtime, SingletonFunction } from '@aws-cdk/aws-lambda'
-import { Construct, Duration, RemovalPolicy } from '@aws-cdk/core'
-import { CustomResource, CustomResourceProvider} from '@aws-cdk/aws-cloudformation'
+import { Construct, RemovalPolicy, CustomResourceProvider, CustomResource, CustomResourceProviderRuntime } from '@aws-cdk/core'
 import { Bucket, BucketProps } from '@aws-cdk/aws-s3'
 import path = require('path')
 
@@ -8,23 +6,81 @@ export class DestroyableBucket extends Bucket {
   constructor(scope: Construct, id: string, props: BucketProps = {}) {
     super(scope, id, {...props})
 
-    const lambda = new SingletonFunction(this, 'DestroyableBucketHandler', {
-      uuid: 'a8e853be-c06c-46c6-ab64-bd61819e992f',
-      runtime: Runtime.NODEJS_10_X,
-      code: Code.asset(path.join(__dirname, '../lambda')),
-      handler: 'main.handler',
-      lambdaPurpose: 'DestroyableBucket',
-      timeout: Duration.minutes(15)
+    const serviceToken = CustomResourceProvider.getOrCreate(this, 'Custom::DestroyableBucket', {
+      codeDirectory: path.join(__dirname, '..', 'lambda'),
+      runtime: CustomResourceProviderRuntime.NODEJS_12,
+      policyStatements: [
+        {
+          Effect: 'Allow',
+          Resource: this.bucketArn,
+          Action: 's3:DeleteObject'
+        },
+        {
+          Effect: 'Allow',
+          Resource: this.bucketArn,
+          Action: 's3:DeleteObjectTagging'
+        },
+        {
+          Effect: 'Allow',
+          Resource: this.bucketArn,
+          Action: 's3:DeleteObjectVersion'
+        },
+        {
+          Effect: 'Allow',
+          Resource: this.bucketArn,
+          Action: 's3:DeleteObjectVersionTagging'
+        },
+        {
+          Effect: 'Allow',
+          Resource: this.bucketArn,
+          Action: 's3:GetBucketLogging'
+        },
+        {
+          Effect: 'Allow',
+          Resource: this.bucketArn,
+          Action: 's3:GetBucketNotification'
+        },
+        {
+          Effect: 'Allow',
+          Resource: this.bucketArn,
+          Action: 's3:GetBucketObjectLockConfiguration'
+        },
+        {
+          Effect: 'Allow',
+          Resource: this.bucketArn,
+          Action: 's3:GetObject'
+        },
+        {
+          Effect: 'Allow',
+          Resource: this.bucketArn,
+          Action: 's3:GetObjectVersion'
+        },
+        {
+          Effect: 'Allow',
+          Resource: this.bucketArn,
+          Action: 's3:ListBucket'
+        },
+        {
+          Effect: 'Allow',
+          Resource: this.bucketArn,
+          Action: 's3:ListBucketVersions'
+        },
+        {
+          Effect: 'Allow',
+          Resource: this.bucketArn,
+          Action: 's3:PutBucketLogging'
+        },
+        {
+          Effect: 'Allow',
+          Resource: this.bucketArn,
+          Action: 's3:PutObject'
+        },
+      ]
     })
 
-    const provider = CustomResourceProvider.lambda(lambda)
-
-    // allow the bucket contents to be read and deleted by the lambda
-    this.grantReadWrite(lambda)
-
     new CustomResource(this, 'DestroyableBucket', {
-      provider,
       resourceType: 'Custom::DestroyableBucket',
+      serviceToken,
       properties: {
         bucketName: this.bucketName,
         removalPolicy: props.removalPolicy || RemovalPolicy.RETAIN
