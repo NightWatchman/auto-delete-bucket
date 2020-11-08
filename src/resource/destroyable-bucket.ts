0437105bc3d832abf71589a235b3b7d8ed68a155
Eric Rushing <rushinge@ucsb.edu>
2020-11-08 13:13:49 -0800

    Use unique ids for each destroyable bucket provider instance
    Fixes #2

--- src/resource/destroyable-bucket.ts
+++ src/resource/destroyable-bucket.ts
@@ -3,10 +3,17 @@ import { Bucket, BucketProps } from '@aws-cdk/aws-s3'
 import path = require('path')
 
 export class DestroyableBucket extends Bucket {
+  // TODO: Figure out a way to share the same provider, but append
+  // the permissions for each invocation, so that when two or more
+  // instances of DestroyableBucket are created the single provider
+  // has the permissions to delete all buckets
   constructor(scope: Construct, id: string, props: BucketProps = {}) {
     super(scope, id, {...props})
 
-    const serviceToken = CustomResourceProvider.getOrCreate(this, 'Custom::DestroyableBucket', {
+    // The bucket id is included in the provider id here, so that
+    // it creates a unique provider for each bucket that has permissions
+    // to delete the files in that bucket.
+    const serviceToken = CustomResourceProvider.getOrCreate(this, `DestroyableBucketProvider${id}`, {
       codeDirectory: path.join(__dirname, '..', 'lambda'),
       runtime: CustomResourceProviderRuntime.NODEJS_12,
       policyStatements: [
@@ -23,7 +30,7 @@ export class DestroyableBucket extends Bucket {
       ]
     })
 
-    new CustomResource(this, 'DestroyableBucket', {
+    new CustomResource(this, `DestroyableBucket${id}`, {
       resourceType: 'Custom::DestroyableBucket',
       serviceToken,
       properties: {
